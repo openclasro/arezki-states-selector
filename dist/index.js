5c779bbe038f7a7ea84df643fa484b2f41d24e6d
arezki <zaidi_arezki@ymail.com>
2022-11-14 12:35:43 +0100

    jsdoc

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,28 @@ var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+/**
+ * Component to select States
+ *
+ * @component
+ * @param {Object} props props
+ * @param {{ abbreviation: string, name: string }[]} props.states array of States
+ * @param {string} props.state currently selected state (abbreviation)
+ * @param {(state: string) => void} props.onSelect fired when user selects a state
+ * @returns {JSX.Element}  A StateSelector component
+ * @example
+ * const STATES = const STATES = [
+  {
+    name: "Alabama",
+    abbreviation: "AL",
+  },
+  {
+    name: "Alaska",
+    abbreviation: "AK",
+  }]
+ * const [state, setState] = useState("NY")
+ * <StateSelector states={STATES} state={state} onSelect={(state) => setState(state)} />
+ */
 function StateSelector(_ref) {
   var state = _ref.state,
       states = _ref.states,
--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 
 import React from 'react'
 
+/**
+ * Component to select States
+ *
+ * @component
+ * @param {Object} props props
+ * @param {{ abbreviation: string, name: string }[]} props.states array of States
+ * @param {string} props.state currently selected state (abbreviation)
+ * @param {(state: string) => void} props.onSelect fired when user selects a state
+ * @returns {JSX.Element}  A StateSelector component
+ * @example
+ * const STATES = const STATES = [
+  {
+    name: "Alabama",
+    abbreviation: "AL",
+  },
+  {
+    name: "Alaska",
+    abbreviation: "AK",
+  }]
+ * const [state, setState] = useState("NY")
+ * <StateSelector states={STATES} state={state} onSelect={(state) => setState(state)} />
+ */
 export default function StateSelector({ state, states, onSelect }) {
     return (
       <div>
